b535f75aec75aaa3c90e1c17443869fbd877ba79
xiaowu <2840432467@qq.com>
2021-10-27 09:50:29 +0800

    get set

--- src/main/java/com/demo/pojo/User.java
+++ src/main/java/com/demo/pojo/User.java
@@ -7,6 +7,63 @@ public class User {
     private String upassword;
     private String sex;
     private String sss;
+    private double uage;
+    private String sdsds;
 
 
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getUpassword() {
+        return upassword;
+    }
+
+    public void setUpassword(String upassword) {
+        this.upassword = upassword;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getSss() {
+        return sss;
+    }
+
+    public void setSss(String sss) {
+        this.sss = sss;
+    }
+
+    public double getUage() {
+        return uage;
+    }
+
+    public void setUage(double uage) {
+        this.uage = uage;
+    }
+
+    public String getSdsds() {
+        return sdsds;
+    }
+
+    public void setSdsds(String sdsds) {
+        this.sdsds = sdsds;
+    }
 }
